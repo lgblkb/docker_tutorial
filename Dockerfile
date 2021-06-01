3b4f236c56deffe66e8ea609cd196e0728e7cd5e
lgblkb <dbakhtiyarov@nu.edu.kz>
2021-06-01 15:28:25 +0600

    Minimal working example using Flask inside Docker

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM python:3
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+ENV FLASK_APP=main
+
+COPY main.py .
+
+CMD [ "python", "-m" , "flask", "run", "--host=0.0.0.0"]
---
+++ main.py
@@ -0,0 +1,13 @@
+from pprint import pformat
+
+from flask import Flask
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+
+
+@app.route("/hello")
+def hello_world():
+    vals = np.random.rand(10, 2)
+    return dict(vals=str(vals), qwe=123, message='Salem')
